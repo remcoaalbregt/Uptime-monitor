c5d99e9ef541d3b71ba86e4ecc37afed0af38193
Remco Aalbregt <remcoaalbregt@gmail.com>
2018-07-14 18:52:50 +0200

    fix build light to initial working

--- build-light/src/config.h
+++ build-light/src/config.h
@@ -1,12 +1,20 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#define BAUD_RATE 9600
+
 #define SERVER_ADDRESS 1
 #define CLIENT_ADDRESS 2
 
 #define RED       2
-#define YELLOW    3
-#define GREEN     4
+#define YELLOW    4
+#define GREEN     3
 #define BEEP_PIN  5
+
+#define SERVER_COMMAND_ALARM    97
+#define SERVER_COMMAND_WARNING  97
+#define SERVER_COMMAND_GOOD     97
+#define SERVER_COMMAND_LUNCH    97
+#define SERVER_COMMAND_DEMO     97
 
 #endif
--- build-light/src/main.cpp
+++ build-light/src/main.cpp
@@ -1,188 +1,130 @@
-// #include <Arduino.h>
-// #include <config.h>
-// #include <RHReliableDatagram.h>
-// #include <RH_NRF24.h>
-// #include <SPI.h>
-
-// RH_NRF24 driver;
-// RHReliableDatagram manager(driver, CLIENT_ADDRESS);
-
-// static const uint8_t allLights[] = {RED,YELLOW,GREEN};
-// uint8_t numberOfLights = sizeof(allLights);
-
-// uint8_t data[] = "Hello World!";
-// uint8_t buffer[RH_NRF24_MAX_MESSAGE_LEN];
-
-// enum Command {ALARM, WARNING, GOOD, LUNCH, DEMO};
-
-// void setLights(uint8_t colors[], uint8_t state) {
-//   uint8_t length = sizeof(colors);
-
-//   for (int colorIndex = 0; colorIndex < length; colorIndex++) {
-//     Serial.print("setting color: ");
-//     Serial.print(colors[colorIndex]);
-//     Serial.print(" to: ");
-//     Serial.println(state);
-//     digitalWrite(colors[colorIndex], state);
-//   }
-// }
-
-// void multipleOn(uint8_t colors[]) {
-//   setLights(colors, HIGH);
-// }
-
-// void multipleOff(uint8_t colors[]) {
-//   setLights(colors, LOW);
-// }
-
-// void allOff() {
-//   multipleOff(allLights);
-// }
-
-// void switchLight(uint8_t color) {
-//   allOff();
-//   digitalWrite(color, HIGH);
-// }
-
-// void flashLight(uint8_t color, uint8_t numberOfTimes, uint16_t speed, bool beep = false) {
-//   allOff();
-//   for(int count = 0; count < numberOfTimes; count++) {
-//     Serial.print("Flash ");
-//     Serial.println(count);
-//     digitalWrite(color, HIGH);
-//     delay(speed);
-//     digitalWrite(color, LOW);
-//     delay(speed);
-//   }
-//   digitalWrite(color, LOW);
-// }
-
-// void flashFast(uint8_t color, uint8_t numberOfTimes, bool beep = false) {
-//   flashLight(color, numberOfTimes, 500);
-// }
-
-// void flashSlow(uint8_t color, uint8_t numberOfTimes, bool beep = false) {
-//   flashLight(color, numberOfTimes, 1000);
-// }
-
-// void handleCommand(Command command) {
-//   Serial.print("Received command: ");
-//   Serial.println(command);
-
-//   switch (command) {
-//     case ALARM:
-//       flashFast(RED, 5, true);
-//       switchLight(RED);
-//       break;
-//     case WARNING:
-//       flashSlow(YELLOW, 3, true);
-//       switchLight(YELLOW);
-//       break;
-//     case GOOD:
-//       switchLight(GREEN);
-//       break;
-//     case LUNCH:
-//       // statements
-//       break; 
-//     case DEMO:
-//       // statements
-//       break;           
-//     default:
-//       Serial.println("Command unknown");
-//   }
-// }
-
-// void setup() {
-//   Serial.begin(9600);
-
-//   if (!manager.init()) { Serial.println("init failed"); }
-
-//   for (int thisPin = 0; thisPin < numberOfLights; thisPin++) {
-//     pinMode(allLights[thisPin], OUTPUT);
-//   }
-
-//   Serial.println("Setup complete..");  
-// }
-
-// void endLoop(const String message) {
-//   Serial.println(message);
-//   delay(500);
-// }
-
-// void loop() {
-//   Serial.println("Sending to nrf24_reliable_datagram_server");
-    
-//   // Send a message to manager_server
-//   if (!manager.sendtoWait(data, sizeof(data), SERVER_ADDRESS)) {
-//     endLoop("sendtoWait failed");
-//     return;
-//   }
-
-//   // Now wait for a reply from the server
-//   uint8_t length = sizeof(buffer);
-//   uint8_t from;   
-//   if (!manager.recvfromAckTimeout(buffer, &length, 2000, &from)) {
-//     endLoop("No reply, is nrf24_reliable_datagram_server running?");
-//     return;
-//   }
-    
-//   Serial.print("got reply from : 0x");
-//   Serial.print(from, HEX);
-//   Serial.print(": ");
-//   Serial.println((char*)buffer);
-
-//   // uint8_t buffer[RH_NRF24_MAX_MESSAGE_LEN];
-//   // uint8_t length = sizeof(buffer);
-
-//   // if (nrf24.waitAvailableTimeout(500)) {
-//   //   // Should be a reply message for us now   
-//   //   if (nrf24.recv(buffer, &length)) {
-//   //     Serial.print("received command: ");
-//   //     Serial.println((char*)buffer);
-
-//   //     // handleCommand(ALARM);
-//   //   } else {
-//   //     Serial.println("recv failed");
-//   //   }
-//   // }
-//   // delay(400);
-// }
-
+#include <Arduino.h>
+#include <config.h>
 #include <RHReliableDatagram.h>
 #include <RH_NRF24.h>
 #include <SPI.h>
 
-#define CLIENT_ADDRESS 1
-#define SERVER_ADDRESS 2
-
 RH_NRF24 driver;
 RHReliableDatagram manager(driver, SERVER_ADDRESS);
 
+static const uint8_t allLights[] = {
+  RED,
+  YELLOW,
+  GREEN
+};
+
+enum Command {ALARM = '1', WARNING = '2', GOOD = '3', LUNCH = '4', DEMO = '5'};
+
+void setLights(uint8_t colors[], uint8_t state) {
+  uint8_t length = sizeof(colors);
+
+  for (int colorIndex = 0; colorIndex < length; colorIndex++) {
+    Serial.print("setting color: ");
+    Serial.print(colors[colorIndex]);
+    Serial.print(" to: ");
+    Serial.println(state);
+    digitalWrite(colors[colorIndex], state);
+  }
+}
+
+void multipleOn(uint8_t colors[]) {
+  setLights(colors, HIGH);
+}
+
+void multipleOff(uint8_t colors[]) {
+  setLights(colors, LOW);
+}
+
+void allOff() {
+  multipleOff(allLights);
+}
+
+void switchLight(uint8_t color) {
+  allOff();
+  digitalWrite(color, HIGH);
+}
+
+void flashLight(uint8_t color, uint8_t numberOfTimes, uint16_t speed, bool beep = false) {
+  allOff();
+  for(int count = 0; count < numberOfTimes; count++) {
+    Serial.print("Flash ");
+    Serial.println(count);
+    digitalWrite(color, HIGH);
+    delay(speed);
+    digitalWrite(color, LOW);
+    delay(speed);
+  }
+  digitalWrite(color, LOW);
+}
+
+void flashFast(uint8_t color, uint8_t numberOfTimes, bool beep = false) {
+  flashLight(color, numberOfTimes, 500);
+}
+
+void flashSlow(uint8_t color, uint8_t numberOfTimes, bool beep = false) {
+  flashLight(color, numberOfTimes, 1000);
+}
+
+void handleCommand(Command command) {
+  Serial.print("Received command: ");
+  Serial.println(command);
+
+  switch (command) {
+    case ALARM:
+      Serial.println("Received Alarm");
+      flashFast(RED, 5, true);
+      switchLight(RED);
+      break;
+    case WARNING:
+      Serial.println("Received Warning");
+      flashSlow(YELLOW, 3, true);
+      switchLight(YELLOW);
+      break;
+    case GOOD:
+      Serial.println("Received Good");
+      switchLight(GREEN);
+      break;
+    case LUNCH:
+      Serial.println("Received Lunch");
+      // statements
+      break; 
+    case DEMO:
+      Serial.println("Received Demo");
+      // statements
+      break;           
+    default:
+      Serial.println("Command unknown");
+  }
+}
+
 void setup() {
-  Serial.begin(9600);
-  if (!manager.init())
-    Serial.println("init failed");
-  // Defaults after init are 2.402 GHz (channel 2), 2Mbps, 0dBm
+  Serial.begin(BAUD_RATE);
+
+  for (int thisPin = 0; thisPin < sizeof(allLights); thisPin++) {
+    pinMode(allLights[thisPin], OUTPUT);
+    digitalWrite(allLights[thisPin], LOW);
+  }
+
+  if (!manager.init()) {
+      Serial.println("init failed");
+  } else {
+    Serial.println("Build light started");
+  }
 }
 
 uint8_t data[] = "Command received";
-// Dont put this on the stack:
-uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
-
+uint8_t buffer[RH_NRF24_MAX_MESSAGE_LEN];
 void loop() {
   if (manager.available()) {
     // Wait for a message addressed to us from the server
-    uint8_t len = sizeof(buf);
+    uint8_t length = sizeof(buffer);
     uint8_t from;
-    if (manager.recvfromAck(buf, &len, &from)) {
-      Serial.print("got request from: ");
-      Serial.print(from, HEX);
-      Serial.print(": ");
-      Serial.println((char*)buf);
-
+    if (manager.recvfromAck(buffer, &length, &from)) {
       // Send a confirmation back to the originator server
-      if (!manager.sendtoWait(data, sizeof(data), from))
+      if (!manager.sendtoWait(data, sizeof(data), from)) {
         Serial.println("sendtoWait failed");
+      }
+      handleCommand(buffer[0]);
     }
   }
 }
